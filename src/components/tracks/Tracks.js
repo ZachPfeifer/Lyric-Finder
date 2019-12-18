77a5c6e3dcf01d9f0e1dbb85e6a5bdaf79d343e0
Zach Pfeifer <mr.zman1@yahoo.com>
2019-12-18 12:22:32 -0700

    added API Key / Took out dummy state

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import { Consumer } from "../../context";
 
 class Tracks extends Component {
   render() {
-    return <h1> Tracks</h1>;
+    return (
+      <Consumer>
+        {value => {
+          return <h1></h1>
+        }}
+      </Consumer>
+    );
   }
 }
 export default Tracks;
--- src/context.js
+++ src/context.js
@@ -1,5 +1,7 @@
 //PROVIDER COMPONENT
 import React, { Component } from "react";
+import axios from "axios";
+
 
 const Context = React.createContext();
 
@@ -7,11 +9,18 @@ export class Provider extends Component {
 
   state = {
     track_list: [
-      { track: { track_name: "ABC" } },
-      { track: { track_name: "123" } }
 
     ],
     heading: "Top 10 Tracks"
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.musixmatch.com/ws/1.1/chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
   }
 
   render() {
