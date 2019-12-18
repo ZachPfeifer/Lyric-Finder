0bce44283b20ccbb230bf0fb288a4f753f3c9beb
Zach Pfeifer <mr.zman1@yahoo.com>
2019-12-18 14:47:54 -0700

    Pre-debugging Lyrics

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from "axios";
+import Moment from "react-moment";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
 
@@ -17,10 +18,13 @@ class Lyrics extends Component {
         `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then(res => {
-        // console.log(res.data)
-        this.setState({ lyrics: res.data.message.body.lyrics })
+        console.log(res.data)
+        this.setState({ lyrics: res.data.message.body.lyrics });
+
         return axios
-          `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+          .get(
+            `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+          )
       })
       .then(res => {
         this.setState({ track: res.data.message.body.track })
@@ -36,28 +40,55 @@ class Lyrics extends Component {
     const { track, lyrics } = this.state
 
 
-    if (track === undefined || Object.keys(track).length === 0 || lyrics === undefined || Object.keys(lyrics).length === 0) {
-      return <Spinner />
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
     } else {
       return (
-        <React.Fragment>
-          <Link
-            to="/"
-            className="btn btn dark btn-sm mb-4">
-            <i className=""></i>
-            Back
-            </Link>
+        <>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+        </Link>
           <div className="card">
             <h5 className="card-header">
-              {track.track_name} by{' '} <span className="text-secondary">{track.artist_name}</span>
+              {track.track.track_name} by{" "}
+              <span className="text-secondary">{track.track.artist_name}</span>
             </h5>
             <div className="card-body">
-              <p className="card-text">{lyrics.lyrics_body}</p>
+              <p className="card-text">{lyrics.lyrics.lyrics_body}</p>
             </div>
           </div>
-        </React.Fragment>
-      )
+
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong>:{" "}
+              {track.track.primary_genres.music_genre_list.length === 0
+                ? "NO GENRE AVAILABLE"
+                : track.track.primary_genres.music_genre_list[0].music_genre
+                  .music_genre_name}
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>:{" "}
+              {track.track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>:{" "}
+              <Moment format="MM/DD/YYYY">
+                {track.track.first_release_date}
+              </Moment>
+            </li>
+          </ul>
+        </>
+      );
     }
-  }
+  };
 }
-export default Lyrics
+
+export default Lyrics;
