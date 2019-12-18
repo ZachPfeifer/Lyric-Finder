cd3ee6a89efe9bc75139a3c265bdb385a54b2698
Zach Pfeifer <mr.zman1@yahoo.com>
2019-12-18 12:38:23 -0700

    Made Spinner

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
+import Spinner from "../layout/Spinner";
 
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          return <h1></h1>
+          console.log(value);
+          const { track_list } = value;
+          if (value.track_list === undefined || track_list.length === 0) {
+            return <Spinner />
+          } else {
+            return <h1>Tracks Loaded</h1>
+          }
         }}
       </Consumer>
     );
--- src/context.js
+++ src/context.js
@@ -8,19 +8,23 @@ const Context = React.createContext();
 export class Provider extends Component {
 
   state = {
-    track_list: [
-
-    ],
+    track_list: [],
     heading: "Top 10 Tracks"
   };
 
   componentDidMount() {
     axios
       .get(
-        `https://api.musixmatch.com/ws/1.1/chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+        process.env.REACT_APP_MM_KEY}`
       )
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err))
+      .then(res => {
+        // console.log(res.data)
+        this.setState({ track_list: res.data.message.body.track_list })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   render() {
