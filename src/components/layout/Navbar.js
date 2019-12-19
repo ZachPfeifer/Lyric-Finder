fc695b12486f31b774e2d971d62d3367064ec5df
Zach Pfeifer <mr.zman1@yahoo.com>
2019-12-19 10:41:00 -0700

    Added Search // Started conditional button

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  //TODO CONVERT TO RCC 
+  // state = {
+  // visible: true
+  // }
+  // render(
   return (
     <nav className="navbar navbar-dark bg-dark mb-5">
+      <Link
+        to="/"
+        className="btn btn-dark btn-sm"
+      >
+        <i className="fas fa-caret-square-left fa-2x"></i>
+      </Link>
       <span className="navbar-brand mb-0 h1 mx-auto">LyricFinder</span>
     </nav>
   );
 };
+
+
 export default Navbar;
--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -40,55 +40,42 @@ class Lyrics extends Component {
     const { track, lyrics } = this.state
 
 
-    if (
-      track === undefined ||
-      lyrics === undefined ||
-      Object.keys(track).length === 0 ||
-      Object.keys(lyrics).length === 0
-    ) {
-      return <Spinner />;
+    if (track === undefined || Object.keys(track).length === 0 || lyrics === undefined || Object.keys(lyrics).length === 0) {
+      return <Spinner />
     } else {
       return (
-        <>
-          <Link to="/" className="btn btn-dark btn-sm mb-4">
-            Go Back
-        </Link>
+        <React.Fragment>
+          <Link
+            to="/"
+            className="btn btn dark btn-sm mb-4">
+            <i className=""></i>
+            Back
+            </Link>
           <div className="card">
             <h5 className="card-header">
-              {track.track.track_name} by{" "}
-              <span className="text-secondary">{track.track.artist_name}</span>
+              {track.track_name} by{' '} <span className="text-secondary">{track.artist_name}</span>
             </h5>
             <div className="card-body">
-              <p className="card-text">{lyrics.lyrics.lyrics_body}</p>
+              <p className="card-text">{lyrics.lyrics_body}</p>
             </div>
           </div>
-
           <ul className="list-group mt-3">
             <li className="list-group-item">
-              <strong>Album ID</strong>: {track.track.album_id}
+              <strong> Album ID</strong>: {track.album_id}
             </li>
             <li className="list-group-item">
-              <strong>Song Genre</strong>:{" "}
-              {track.track.primary_genres.music_genre_list.length === 0
-                ? "NO GENRE AVAILABLE"
-                : track.track.primary_genres.music_genre_list[0].music_genre
-                  .music_genre_name}
+              <strong>Song Genre</strong>: {' '}{track.primary_genre.music_genre_list[0].music_genre.music_genre_name}
             </li>
             <li className="list-group-item">
-              <strong>Explicit Words</strong>:{" "}
-              {track.track.explicit === 0 ? "No" : "Yes"}
+              <strong>Explicit Words</strong>: {' '}{track.explicit === 0 ? 'No' : 'Yes'}
             </li>
             <li className="list-group-item">
-              <strong>Release Date</strong>:{" "}
-              <Moment format="MM/DD/YYYY">
-                {track.track.first_release_date}
-              </Moment>
+              <strong>Release Date</strong>:{' '} <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
             </li>
           </ul>
-        </>
-      );
+        </React.Fragment>
+      )
     }
-  };
+  }
 }
-
-export default Lyrics;
+export default Lyrics
